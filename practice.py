bf5668ed81171c5aece29c63627c35d206e5706c
sentryc0201 <84361475+sentryc0201@users.noreply.github.com>
2021-09-24 08:54:01 +0900

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,11 @@
+li = [{'Name':'Song', 'Age':10}, {'Name':'Song', 'Age':20}, {'Name':'Kim', 'Age':10}]
+
+print(li)
+for i in li
+    if i['Name'] == 'Song':
+        li.remove(i)
+        break
+
+
+
+print(li)
